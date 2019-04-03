5591cf1be7712b8540b2d7318b74b309f3763f4b
Walker Wilcoxon <walker.wilcoxon@gmail.com>
2019-04-03 11:42:41 -0500

    [refactor] Added page files that werent added in commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ import 'package:http/http.dart' as http;
 import 'package:lfdl_app/gps.dart';
 import 'dart:convert';
 import 'dart:io';
+import 'package:lfdl_app/pages/map_page.dart';
+import 'package:lfdl_app/pages/test_page.dart';
+import 'package:lfdl_app/pages/report_page.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -27,85 +31,11 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  static const String route = '/';
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-
-
-class _HomePageState extends State<HomePage> {
-  var httpResponse = "";
-
-  final mapController = MapController();
-
-  @override
-  void initState() {
-    super.initState();
-    _centerMap();
-  }
-
-  void _httpGetRequest() async {
-    final url = "https://stupidcpu.com/api/ping";
-    HttpClient httpClient = new HttpClient();
-    HttpClientRequest request = await httpClient.getUrl(Uri.parse(url));
-    HttpClientResponse response = await request.close();
-    final reply = await response.transform(utf8.decoder).join();
-    httpClient.close();
-    setState(() {
-      httpResponse = reply;
-    });
-  }
-
-  void _centerMap() async {
-    final position = await GPS().location();
-    mapController.move(LatLng(position.latitude, position.longitude), 15.0);
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home")),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            FlatButton(
-              child: Text("Send Request"),
-              onPressed: _httpGetRequest,
-            ),
-            FlatButton(
-              child: Text("Center Map"),
-              onPressed: _centerMap,
-            ),
-            Text("Ping: $httpResponse"),
-            Flexible(
-                child: FlutterMap(
-              mapController: mapController,
-              options: MapOptions(
-                center: LatLng(51.5, -0.09),
-                zoom: 5.0,
-              ),
-              layers: [
-                TileLayerOptions(
-                    urlTemplate:
-                        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                    subdomains: ['a', 'b', 'c']),
-              ],
-            )),
-          ],
-        ),
-      ),
+      home: MapPage(),
+      routes: <String, WidgetBuilder>{
+        ReportPage.route: (context) => ReportPage(),
+        TestPage.route: (context) => TestPage(),
+      },
     );
   }
 }
