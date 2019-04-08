977b827d6982a61631bb550afb10048c8fdb5d1c
surao4813 <surao4813@gmail.com>
2019-04-08 11:35:14 -0500

    [update] Added buttons for snow, ice, and slush reporting and  map.

--- lib/pages/report_page.dart
+++ lib/pages/report_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import '../drawer.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:lfdl_app/gps.dart';
+import 'package:latlong/latlong.dart';
+
 
 
 //Self-reporting page
@@ -14,12 +18,82 @@ class ReportPage extends StatefulWidget {
 
 class ReportPageState extends State<ReportPage> {
 
+  int snowCounter;
+  int iceCounter;
+  int slushCounter;
+
+  final mapController = MapController();
+  FlutterMap _map;
+  GPS gps = GPS();
+
+  void center() async {
+    final position = await gps.location();
+    mapController.move(position, 10.0);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Report")),
+      appBar: AppBar(
+        title: Text("Report"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.looks_one),
+            tooltip: 'Report snow',
+            onPressed: (){
+              setState(() {
+                snowCounter++;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.looks_two),
+            tooltip: 'Report ice',
+            onPressed: (){
+              setState(() {
+                iceCounter++;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.looks_3),
+            tooltip: 'Report slush',
+            onPressed: (){
+              setState(() {
+                slushCounter++;
+              });
+            },
+          )
+        ],
+      ),
       drawer: buildDrawer(context, ReportPage.route),
-      body: Text("Report"),
+      body: new Padding(
+        padding: new EdgeInsets.all(8.0),
+        child: new Column(
+          children: [
+            new Padding(
+              padding: new EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: new Text("This is a map that is showing (51.5, -0.9)."),
+            ),
+            new Flexible(
+              child: new FlutterMap(
+                mapController: mapController,
+                options: MapOptions(
+                  center: LatLng(51.5, -0.09),
+                  zoom: 5.0,
+                ),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate:
+                      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                      subdomains: ['a', 'b', 'c']),
+                ],
+              ),
+            ),
+            new Text("$snowCounter, " + "$iceCounter, " + "$slushCounter")
+          ],
+        ),
+      ),
     );
   }
 }
